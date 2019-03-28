7a5a511089531091fbd179cc3f88f80d9c1a135b
Codrin Popescu <codrin@ascheri.co.uk>
2019-03-28 10:45:58 +0000

    Extract redundency

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
 class ArticlesController < ApplicationController
-	def index
-		@articles = Article.all		
-	end
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
+
+  def index
+	@articles = Article.all		
+  end
   def new
   	@article = Article.new
   end
 
   def edit
-  	@article = Article.find(params[:id])
-  	
+  	 	
   end
 
   def create
@@ -22,8 +23,7 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-  	@article = Article.find(params[:id])
-  	if @article.update(article_params)
+  	 	if @article.update(article_params)
   	  flash[:notice] = "Article Updated"
   	  redirect_to article_path(@article)
   	else
@@ -33,11 +33,10 @@ class ArticlesController < ApplicationController
   end
 
   def show
-  	@article = Article.find(params[:id])
+
   end
 
   def destroy
-  	@article = Article.find(params[:id])
   	@article.destroy
   	flash[:notice] = "Article deleted"
   	redirect_to articles_path
@@ -45,6 +44,9 @@ class ArticlesController < ApplicationController
   end
 
   private
+  def set_article
+  	@article = Article.find(params[:id])
+  end
   def article_params
   	params.require(:article).permit(:title, :description)
   end
