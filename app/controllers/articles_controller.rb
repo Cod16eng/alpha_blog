400c00ba7a4c6c46eb9abc0718f06f5cdbc84b46
Codrin Popescu <codrin@ascheri.co.uk>
2019-03-28 09:25:43 +0000

    Added Edit And Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
   def new
   	@article = Article.new
-  end 
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
+  	
+  end
 
   def create
   	#render plain: params[:article].inspect
@@ -12,6 +17,16 @@ class ArticlesController < ApplicationController
     else
   	  render 'new'
     end
+  end
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+  	  flash[:notice] = "Article Updated"
+  	  redirect_to article_path(@article)
+  	else
+  		render 'edit'  		
+  	end
+  	
   end
 
   def show
