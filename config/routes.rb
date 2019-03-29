3605ace0541abb53dd89b47b2ae56de441aa737b
Codrin Popescu <codrin@ascheri.co.uk>
2019-03-29 16:22:42 +0000

    Signup users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   get 'about', to:'pages#about'
+  get 'signup', to:'users#new'
 
   root 'pages#home'
 
   resources :articles
+
+  post 'users', to: 'users#create'
+
+  resources :users, except: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
